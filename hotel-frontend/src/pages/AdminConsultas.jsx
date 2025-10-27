f842e446a5ed1bfdcf53826c4d25ddc98d6a228c
martinzm01 <martinzrm2001@gmail.com>
2025-10-27 01:29:48 -0300

    Pagina AdminConsultas y su tabla.

--- hotel-frontend/src/pages/AdminConsultas.jsx
+++ hotel-frontend/src/pages/AdminConsultas.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import HotelHeader from "../components/headerHabitaciones";
-import PageBreadcrumb from "../components/PageBreadCrumb";
-import ComponentCard from "../components/ComponentCard";
-import TableAdminReservas from "../components/TableAdminReservas";
+import TableAdminCosulta from "../components/TableAdminconsultas";
 export default function AdminConsultas() {
 
 
@@ -12,9 +10,7 @@ export default function AdminConsultas() {
             <HotelHeader/>
             <div className="p-6 min-h-screen  bg-background  bg-gray-50">
                 <div className="space-y-6">
-                <ComponentCard title="Reservas">
-                    <TableAdminReservas />
-                </ComponentCard>
+                    <TableAdminCosulta />
                 </div>
             </div>
             
