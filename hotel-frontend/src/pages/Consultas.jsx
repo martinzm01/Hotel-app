044ee6d7ae507b3f352258bc95e5603d2f303180
martinzm01 <martinzrm2001@gmail.com>
2025-11-07 17:42:55 -0300

    Update Consultas.jsx

--- hotel-frontend/src/pages/Consultas.jsx
+++ hotel-frontend/src/pages/Consultas.jsx
@@ -331,9 +331,7 @@ export default function Consultas() {
                         <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                           Respuesta
                         </th>
-                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                          Respondida por
-                        </th>
+
                       </tr>
                     </thead>
                     <tbody>
